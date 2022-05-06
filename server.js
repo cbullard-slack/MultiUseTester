98999988deaddee0bb97ca84a37440763ce6cfc7
Callum Bullard <cbullard@slack-corp.com>
2022-05-06 12:18:28 +1000

    Parse the payload body to JSON

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //Start of the Node Express Template
 const express = require("express");
-const int = require("./public/interaction");
-const axios  = require("axios");
+const int = require("./services/interaction");
+const axios = require("axios");
 const app = express();
 
 const v1 = express.Router();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/slack/v1", v1);
 
@@ -19,19 +19,20 @@ app.get("/", (req, res) => {
   req.sendFile(__dirname + "/views/404.html");
 });
 
-v1.post("/interactive", (req,res) => {
-  console.log(req.body)
-  res.sendStatus(200)
+v1.post("/interactive", (req, res) => {
+  const body = JSON.parse(req.body);
+  console.log(body);
+  res.sendStatus(200);
 });
 
-v1.post("/clear",(req,res) => {
-  console.log(req.body)
-  res.status(200).send(areYouSureDelete)
+v1.post("/clear", (req, res) => {
+  console.log(req.body);
+  res.status(200).send(areYouSureDelete);
 });
 
-v1.post("/auth", (req,res) => {
-  console.log(req.body)
-  res.sendStatus(200)
+v1.post("/auth", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 // listen for requests :)
@@ -42,41 +43,41 @@ const listener = app.listen(process.env.PORT, () => {
 // Response Templates
 
 const areYouSureDelete = {
-  "blocks": [
+  blocks: [
     {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": ":warning: This will delete *all messages* in this channel! :warning:\n\nAre you sure you wish to proceed?"
-      }
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":warning: This will delete *all messages* in this channel! :warning:\n\nAre you sure you wish to proceed?",
+      },
     },
     {
-      "type": "divider"
+      type: "divider",
     },
     {
-      "type": "actions",
-      "elements": [
+      type: "actions",
+      elements: [
         {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Decline",
-            "emoji": true
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Decline",
+            emoji: true,
           },
-          "style": "primary",
-          "action_id": "actionId-0"
+          style: "primary",
+          action_id: "actionId-0",
         },
         {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Confirm",
-            "emoji": true
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Confirm",
+            emoji: true,
           },
-          "style": "danger",
-          "action_id": "Confirm"
-        }
-      ]
-    }
-  ]
-}
+          style: "danger",
+          action_id: "Confirm",
+        },
+      ],
+    },
+  ],
+};
